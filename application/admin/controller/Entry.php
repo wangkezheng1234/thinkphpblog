4ff7bd4cf06a4949469b343660e067ab4cd4b41b
kezheng.wang <kezheng.wang@verystar.cn>
2018-07-27 20:26:52 +0800

    密码修改功能(页面、验证、清除session）

--- application/admin/controller/Entry.php
+++ application/admin/controller/Entry.php
@@ -3,10 +3,23 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use app\common\model\Admin;
 
 class Entry extends Common
 {
     public function index(){
+        return $this->fetch();
+    }
+    public function pass(){
+        if(request()->isPost()){
+            $rs = (new Admin())->pass(input('post.'));
+            if($rs['valid']){
+                session(null);
+                $this->success($rs['msg'],'admin/entry/index');
+            }else{
+                $this->error($rs['msg']);exit;
+            }
+        }
         return $this->fetch();
     }
 }
--- application/common/model/Admin.php
+++ application/common/model/Admin.php
@@ -4,6 +4,7 @@ namespace app\common\model;
 
 use think\Model;
 use think\Loader;
+use think\Validate;
 
 class Admin extends Model
 {
@@ -27,5 +28,36 @@ class Admin extends Model
         session('admin.admin_username',$rs['admin_username']);
         return ['valid'=>1,'msg'=>'登录成功'];
         
+    }
+    public function pass($data){
+       #数据格式是否正确
+        $validate = new Validate(
+            [
+                'password'=>'require',
+                'new_password'=>'require',
+                'confirm_password'=>'require|confirm:new_password',
+            ],
+            [
+                'password.require'=>'原始密码不能为空',
+                'new_password.require'=>'新密码不能为空',
+                'confirm_password.require'=>'确认密码不能为空',
+                'confirm_password.confirm'=>'原始密码和新密码不一致',
+            ]
+            );
+        #检测原始密码是否正确
+        if(!$validate->check($data)){
+            return ['valid'=>0,'msg'=>$validate->getError()];
+        }
+        $rs = $this->where('admin_id',session('admin.admin_id'))->where('admin_password',$data['password'])->find();
+        if(!$rs){
+            return ['valid'=>0,'msg'=>'原始密码数据'];
+        }
+        #修改密码
+        $rs = $this->save(['admin_password'=>$data['new_password']],[$this->pk=>session('admin.admin_id')]);
+        if(!$rs){
+            return ['valid'=>0,'msg'=>'密码修改失败'];
+        }else{
+            return ['valid'=>1,'msg'=>'密码修改成功'];
+        }
     }
 }

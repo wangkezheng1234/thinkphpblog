a19b4db7812f2d8b59c3e49bfd9ea44ee5b048b4
kezheng.wang <kezheng.wang@verystar.cn>
2018-07-28 16:05:54 +0800

    添加栏目修改

---
+++ application/admin/controller/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Category extends Controller
+{
+    //
+    public function index(){
+        return $this->fetch();
+    }
+}
